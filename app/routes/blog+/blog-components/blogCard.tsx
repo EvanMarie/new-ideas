1069775c6f88b7fd227e0b48f8567234a4d75739
Evan Marie Carr <evanmariecarr@gmail.com>
2024-10-04 10:58:34 -0400

    blog

--- app/routes/blog+/blog-components/blogCard.tsx
+++ app/routes/blog+/blog-components/blogCard.tsx
@@ -1,7 +1,13 @@
 import { ScrollTransition } from "~/mainDesignComponents/scrollTransition";
 import { BlogPost } from "../blog-posts/blogsArray";
 import { NavLink } from "@remix-run/react";
-import { Box, Flex, FlexFull } from "~/buildingBlockComponents/mainContainers";
+import {
+  Box,
+  Flex,
+  FlexFull,
+  HStackFull,
+  VStackFull,
+} from "~/buildingBlockComponents/mainContainers";
 import Image from "~/buildingBlockComponents/image";
 
 export default function BlogIndexCard({
@@ -12,17 +18,17 @@ export default function BlogIndexCard({
   index: number;
 }) {
   const lgScreenWidths = [
-    "lg:w-65% xxl:w-25%",
-    "lg:w-35% xxl:w-50%",
-    "lg:w-40% xxl:w-25%",
-    "lg:w-60% xxl:w-35%",
-    "lg:w-55% xxl:w-24%",
-    "lg:w-45% xxl:w-40%",
+    "lg:w-65% xxl:w-35%",
     "lg:w-35% xxl:w-40%",
+    "lg:w-40% xxl:w-25%",
+    "lg:w-60% xxl:w-30%",
+    "lg:w-55% xxl:w-30%",
+    "lg:w-45% xxl:w-30%",
+    "lg:w-35% xxl:w-35%",
     "lg:w-65% xxl:w-30%",
+    "lg:w-50% xxl:w-35%",
     "lg:w-50% xxl:w-30%",
-    "lg:w-50% xxl:w-30%",
-    "lg:w-40% xxl:w-40%",
+    "lg:w-40% xxl:w-35%",
     "lg:w-60% xxl:w-30%",
   ];
 
@@ -37,20 +43,25 @@ export default function BlogIndexCard({
     <ScrollTransition
       delay={0.01 * (index + 0.1) + 0.1}
       type="rotate3D"
-      className={`w-full rounded-none ${widthClass} p-1vh`}
+      className={`w-full rounded-none ${widthClass} p-0.5vh`}
     >
-      <FlexFull className="bg-slate-900/80 bg-gradient-to-r from-violet-900/30 via-fuchsia-900/50 to violet-900/30 px-1vh py-0.3vh rounded-none justify-between rounded-2vh">
-        <NavLink to={blog.slug} key={blog.slug}>
-          <span>{blog.title}</span>
-        </NavLink>
-        <Flex className="w-13vh h-13vh flex-shrink-0">
-          <Image
-            src={blog.image}
-            alt={blog.title}
-            className="w-full h-full rounded-2vh"
-          />
-        </Flex>
-      </FlexFull>
+      <NavLink to={blog.slug} key={blog.slug}>
+        <HStackFull className="bg-indigo-300/80 bg-gradient-to-r from-fuchsia-300/50 via-cyan-300/50 to-purple-300/50 justify-between rounded-2vh border-900-md shadowNarrowNormal">
+          <VStackFull className="p-1vh">
+            <span className="text-lg text-slate-950 lightTextShadow font-semibold">
+              {blog.title}
+            </span>
+          </VStackFull>
+
+          <Flex className="w-13vh h-13vh flex-shrink-0">
+            <Image
+              src={blog.image}
+              alt={blog.title}
+              className="w-full h-full rounded-2vh"
+            />
+          </Flex>
+        </HStackFull>{" "}
+      </NavLink>
     </ScrollTransition>
   );
 }
