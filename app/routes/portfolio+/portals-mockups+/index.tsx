9f708d229ef0c034f7eda12aa36e13a341439b72
Evan Marie Carr <evanmariecarr@gmail.com>
2024-10-06 00:28:32 -0400

    portals

--- app/routes/portfolio+/portals-mockups+/index.tsx
+++ app/routes/portfolio+/portals-mockups+/index.tsx
@@ -19,25 +19,30 @@ export default function PortalMockupIndex() {
             to={project.slug}
             className="w-full px-1vh py-0.5vh sm:px-2vh sm:py-1vh"
           >
-            <FlexFull className="bg-indigo-950/60">
-              <HStackFull gap="gap-1.5vh">
-                <Flex className="w-25vh h-25vh flex-shrink-0">
+            <FlexFull className="bg-indigo-950/60 py-2vh ">
+              <FlexFull
+                gap="gap-1.5vh"
+                className="flex-col items-center gap-2vh"
+              >
+                <Flex className="w-90vw h-90vw flex-shrink-0">
                   <Image
                     src={project.thubmnail}
                     alt={project.title}
                     className="h-full w-full"
                   />
                 </Flex>
-                <VStackFull align="items-start">
-                  <Text className="text-xl text-col-500 textShadow">
-                    {project.title}
-                  </Text>
-                  <Text className="text-fuchsia-300 textShadow">
-                    {project.tagline}
-                  </Text>
-                  <Text>{project.description}</Text>
-                </VStackFull>
-              </HStackFull>
+                <FlexFull className="p-1vh">
+                  <VStackFull align="items-start">
+                    <Text className="text-xl text-col-500 textShadow">
+                      {project.title}
+                    </Text>
+                    <Text className="text-fuchsia-300 textShadow">
+                      {project.tagline}
+                    </Text>
+                    <Text>{project.description}</Text>
+                  </VStackFull>
+                </FlexFull>
+              </FlexFull>
             </FlexFull>
           </NavLink>
         ))}
